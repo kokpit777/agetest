f1c31ca2839711b159803a6c791f31398306b054
kokpit <142565931+kokpit777@users.noreply.github.com>
2025-03-12 00:41:02 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if (age <= 12) {
+            System.out.println("Child");
+        } else if (age >= 13 && age <= 17) {
+            System.out.println("Teenager");
+        } else if (age >= 18 && age <= 59) {
+            System.out.println("Adult");
+        } else if (age >= 60) {
+            System.out.println("Senior Citizen");
+        }
+
+        sc.close();
+    }
+}
